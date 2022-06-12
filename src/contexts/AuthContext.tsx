642609793ca59406b05bc29b639056a9a49c85b0
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-12 14:00:07 -0300

    Recuperando estado da autenticacao

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { setCookie } from "nookies";
+import { setCookie, parseCookies } from "nookies";
 import { api } from "../services/api";
 
 type User = {
@@ -30,6 +30,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const isAuthenticated = !!user; //se user for vazio retorna false
 
+  useEffect(() => {
+    const { "nextauth.token": token } = parseCookies();
+
+    if (token) {
+      api.get("/me").then((response) => {
+        const { email, roles, permissions } = response.data;
+        setUser({ email, roles, permissions });
+      });
+    }
+  }, []);
+
   const router = useRouter();
 
   async function signIn({ email, password }: SignInCredentialsProps) {
@@ -39,17 +50,23 @@ export function AuthProvider({ children }: AuthProviderProps) {
       const { token, refreshToken, permissions, roles } = response.data;
 
       //sempre que estiver tentando lidar com cookies (destruir, editar ou criar) do lado do browser
-      //o primeiro parametro deve ser undefined
+      //o primeiro parâmetro deve ser undefined
+
       setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, //quanto tempo fica salvo no browser (no caso 30 dias)
-        path: "/", //qualquer endereco do app tem acesso ao cookie
+        path: "/", //qualquer endereço do app tem acesso ao cookie
       });
+
       setCookie(undefined, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30,
         path: "/",
       });
 
       setUser({ email, permissions, roles });
+
+      //atualiza header com token para usuário conseguir fazer as requisições
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
       router.push("/dashboard");
     } catch (error) {
       console.log(error);
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
+import { parseCookies } from "nookies";
+
+const cookies = parseCookies();
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
+
+  //passa como header o token em todas as requisições
+  headers: {
+    Authorization: `Bearer ${cookies["nextauth.token"]}`,
+  },
 });
