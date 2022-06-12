87604c48b43eaf938ecbbecd16fcf9484bc533b6
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-12 02:01:47 -0300

    Contexto de autenticação

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,31 @@
+import { createContext, ReactNode } from "react";
+
+interface SignInCredentialsProps {
+  email: string;
+  password: string;
+}
+
+type AuthContextData = {
+  signIn(credentials: SignInCredentialsProps): Promise<void>;
+  isAuthenticated: boolean;
+};
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const isAuthenticated = false;
+
+  async function signIn({ email, password }: SignInCredentialsProps) {
+    console.log({ email, password });
+  }
+
+  return (
+    <AuthContext.Provider value={{ signIn, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
