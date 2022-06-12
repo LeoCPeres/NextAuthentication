3350c416bedf49ccb8503ab40e128b732bbbc56d
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-12 02:13:19 -0300

    acesso a api com axios

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode } from "react";
+import { api } from "../services/api";
 
 interface SignInCredentialsProps {
   email: string;
@@ -20,7 +21,12 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const isAuthenticated = false;
 
   async function signIn({ email, password }: SignInCredentialsProps) {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", { email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   return (
