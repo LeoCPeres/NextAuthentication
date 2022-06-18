9a9163f3ad4c031dbf4d59355836b47d8224f2c2
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-17 23:57:05 -0300

    Authentication with SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { parseCookies } from "nookies";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -40,3 +41,22 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+//authentication with SSR
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookies = parseCookies(context);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
