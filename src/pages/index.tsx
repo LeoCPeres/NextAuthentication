c3d50f7c6e8cabcc66b298cc317438b2477babc9
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-19 00:51:51 -0300

    Validando visitante

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { parseCookies } from "nookies";
 import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState("");
@@ -24,16 +24,8 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <form onSubmit={handleSubmit} className={styles.main}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         <button type="submit">Entrar</button>
       </form>
     </div>
@@ -44,19 +36,8 @@ export default Home;
 
 //authentication with SSR
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const cookies = parseCookies(context);
-
-  if (cookies["nextauth.token"]) {
-    return {
-      redirect: {
-        destination: "/dashboard",
-        permanent: false,
-      },
-    };
-  }
-
+export const getServerSideProps: GetServerSideProps = withSSRGuest(async (context) => {
   return {
     props: {},
   };
-};
+});
