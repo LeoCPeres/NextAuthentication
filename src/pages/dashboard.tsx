36e7bae4decfbbe90b8bef3418f9b80754a55635
Leonardo Csik Peres <leonardocsikperes@gmail.com>
2022-06-19 11:30:32 -0300

    Criando hook de permissao

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,6 +1,7 @@
 import { destroyCookie } from "nookies";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
+import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { AuthTokenError } from "../services/errors/AuthTokenError";
@@ -8,6 +9,8 @@ import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const userCanSeeMetrics = useCan({ permissions: ["metrics.list"] });
 
   useEffect(() => {
     api
